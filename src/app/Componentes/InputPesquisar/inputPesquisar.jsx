bca0e52448088cc5882fe26320eef87930e4e861
Your Name <you@example.com>
2024-09-09 13:31:17 -0300

    modificações na barra pesquisar

--- src/app/Componentes/InputPesquisar/inputPesquisar.jsx
+++ src/app/Componentes/InputPesquisar/inputPesquisar.jsx
@@ -2,11 +2,24 @@ import Image from "next/image";
 
 import style from "../InputPesquisar/input.module.css";
 
+import Produtos from "@/app/arrayProdutos/produtosCardapio.js";
+
 export default function BarraPesquisar(props){
+
+    const BuscarProduto = (textoDigitado) =>{
+        return Produtos.filter((produto)=> produto.nome.toLocaleLowerCase().includes(textoDigitado.toLocaleLowerCase()) || 
+         produto.descricao.toLocaleLowerCase().includes(textoDigitado.toLocaleLowerCase())
+        )
+    }
+
     return(
+
         <div className={style.containerInput}>
             <Image className={style.imagemIcon} src={props.imagemIconPesquisar} alt={props.title} title={props.title}/>
-            <input className={style.input} type="text" value={textoDigitado} onChange={event=()=> (event.target.value)} placeholder={props.placeholder} />
+            <input className={style.input} 
+             type="text"
+             onChange={(event) => BuscarProduto(event.target.value)} 
+             placeholder={props.placeholder} />
         </div>
     )
 }
--- src/app/service/service.js
+++ src/app/service/service.js
@@ -1,14 +1,9 @@
-import Dados from "@/app/arrayProdutos/produtosCardapio.js";
+import Produtos from "@/app/arrayProdutos/produtosCardapio.js";
 
-const FiltrarPrato = (Categoria) =>{
-    return Dados.filter((produto)=> produto.categoria === Categoria)
-}
-
-const BuscarProduto = (textoDigitado) =>{
-    return Dados.filter((produto)=> produto.nome.toLocaleLowerCase().includes(textoDigitado.toLocaleLowerCase()) || 
-     produto.descricao.toLocaleLowerCase().includes(textoDigitado.toLocaleLowerCase())
-    )
+const FiltrarPrato = (Categoria) =>{ 
+     Produtos.filter((produto)=> produto.categoria === Categoria)
 }
 
 
-export default FiltrarPrato;
+
+export default {FiltrarPrato};
